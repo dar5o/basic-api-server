3ca8c151f56b000dd03631e82fb14dc9ca4a7a8e
dar5o <87797682+dar5o@users.noreply.github.com>
2021-10-20 18:15:39 -0700

    Update 404.test.js

--- ___tests___/404.test.js
+++ ___tests___/404.test.js
@@ -6,14 +6,14 @@ const request = supertest(server.server);
 
 describe('Given /iDoNotExist', () => {
   describe('When GET', () => {
-    it('Then returns 404 status', async  () => {
+    it('returns 404 status', async  () => {
       const response = await request.get('/iDoNotExist');
       expect(response.status).toStrictEqual(404);
     });
   });
 
   describe('When POST', () => {
-    it('Then returns 404 status', async  () => {
+    it('returns 404 status', async  () => {
       const response = await request.post('/iDoNotExist');
       expect(response.status).toStrictEqual(404);
     });
